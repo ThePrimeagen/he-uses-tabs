b061ffbfe25c99e0f768df3b352a6422001cc4e1
mpaulson <mpaulson@netflix.com>
2022-04-24 10:37:47 -0600

    feat: step 6 and suck it

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -52,10 +52,6 @@ func listenForFires(channel chan<- NamedGameMessage, c *websocket.Conn, name byt
     }()
 }
 
-func gameLoop() chan struct{} {
-    go func() {
-}
-
 func RunGame(connections chan *websocket.Conn) {
 
     for {
@@ -92,26 +88,67 @@ func RunGame(connections chan *websocket.Conn) {
 
             // Steps 5. The rust version has a tokio::select
             ticker := time.NewTicker(time.Millisecond * 16)
-            last_start := time.Now().UnixMicro()
-            select {
-            case fire := <- fires:
-                player := game.Players[0]
-                if fire.name == 'b' {
-                    player = game.Players[1]
-                }
-                game.Bullets = append(game.Bullets, CreateBulletFromPlayer(&player, 1.0))
-            case <- ticker.C:
-                // 6. part 1 : calculate the time difference between each loop.
-                diff := time.Since(last_start).Microseconds()
-                last_start = time.Now()
+            last_start := time.Now()
 
-                // 6. do all the collision / updating
-                for _, bullet := range game.Bullets {
-                    UpdateBullet(&bullet, diff)
-                }
+            var winner Player
+            var loser Player
 
-                stats.AddDelta(last_start);
+            game_me_daddy: for {
+                select {
+                case fire := <- fires:
+                    player := game.Players[0]
+                    if fire.name == 'b' {
+                        player = game.Players[1]
+                    }
+                    game.Bullets = append(game.Bullets, CreateBulletFromPlayer(&player, 1.0))
+                case <- ticker.C:
+                    // 6. part 1 : calculate the time difference between each loop.
+                    diff := time.Since(last_start).Microseconds()
+                    last_start = time.Now()
+
+                    // 6. do all the collision / updating
+                    for _, bullet := range game.Bullets {
+                        UpdateBullet(&bullet, diff)
+                    }
+
+                    // TODO: I may need to replace this because it sucks
+                    loop_me_daddy: for idx1 := 0; idx1 < len(game.Bullets); idx1 += 1 {
+                        bullet := game.Bullets[idx1]
+                        for idx2 := idx1 + 1; idx2 < len(game.Bullets); idx2 += 1 {
+                            bullet2 := game.Bullets[idx2]
+                            if bullet.Geo.HasCollision(&bullet2.Geo) {
+                                last := game.Bullets[len(game.Bullets) - 1]
+                                second := game.Bullets[len(game.Bullets) - 2]
+
+                                game.Bullets[idx1] = last;
+                                game.Bullets[idx2] = second;
+
+                                game.Bullets = game.Bullets[:len(game.Bullets) - 2]
+                                break loop_me_daddy
+                            }
+                        }
+                    }
+
+                    for _, bullet := range game.Bullets {
+                        if game.Players[0].Geo.HasCollision(&bullet.Geo) {
+                            winner = game.Players[1]
+                            loser = game.Players[0]
+                            break game_me_daddy;
+                        }
+                        if game.Players[1].Geo.HasCollision(&bullet.Geo) {
+                            winner = game.Players[0]
+                            loser = game.Players[1]
+                            break game_me_daddy;
+                        }
+                    }
+
+                    stats.AddDelta(diff);
+                }
             }
+
+            // Part 7. Send out the winner / loser message and close down the
+            // suckets
+
         }()
 
     }
